709fe5dd46e67527c41b6c632b0c2e2d22263e9b
Ken Kundert <ken@theKunderts.net>
2020-11-06 20:24:15 -0800

    add testing for new versions of python

--- setup.py
+++ setup.py
@@ -35,11 +35,10 @@ setup(
         'Operating System :: POSIX :: Linux',
         #'Programming Language :: Python :: 3.3',
         #    should work, but is no longer tested
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Utilities',
     ],
 )
