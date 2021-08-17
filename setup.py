612f190e0834ee83e4e91cd36739ac4aa2cbed5d
Ken Kundert <ken@theKunderts.net>
2021-08-17 13:58:51 -0700

    enable zip_safe in installer

--- setup.py
+++ setup.py
@@ -8,26 +8,26 @@ with open('README.rst', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name='rkm_codes',
-    version='0.5.0',
-    description='QuantiPhy support for RKM codes.',
-    long_description=readme,
-    long_description_content_type='text/x-rst',
-    author="Ken Kundert",
-    author_email='rkm_codes@nurdletech.com',
-    packages='rkm_codes'.split(),
-    url='https://nurdletech.com/linux-utilities/rkm_codes',
-    download_url='https://github.com/kenkundert/rkm_codes/tarball/master',
-    license='GPLv3+',
-    zip_safe=True,
-    install_requires='quantiphy>=2.4'.split(),
-    setup_requires='pytest-runner>=2.0'.split(),
-    tests_require='pytest'.split(),
-    keywords=[
+    name = 'rkm_codes',
+    version = '0.5.0',
+    description = 'QuantiPhy support for RKM codes.',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
+    author = "Ken Kundert",
+    author_email = 'rkm_codes@nurdletech.com',
+    packages = 'rkm_codes'.split(),
+    url = 'https://nurdletech.com/linux-utilities/rkm_codes',
+    download_url = 'https://github.com/kenkundert/rkm_codes/tarball/master',
+    license = 'GPLv3+',
+    zip_safe = True,
+    install_requires = 'quantiphy>=2.4'.split(),
+    setup_requires = 'pytest-runner>=2.0'.split(),
+    tests_require = 'pytest'.split(),
+    keywords = [
         'rkm codes',
         'quantiphy',
     ],
-    classifiers=[
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
