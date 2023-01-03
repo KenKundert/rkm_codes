e8c939126e8dbe547425c59e8300e3178613c2b2
Ken Kundert <ken@theKunderts.net>
2023-01-02 16:54:53 -0800

    update copyright

--- rkm_codes/rkm_codes.py
+++ rkm_codes/rkm_codes.py
@@ -34,7 +34,7 @@ types, such as voltage, current, and inductance.
 """
 
 # license {{{1
-# Copyright (C) 2018-2022 Kenneth S. Kundert
+# Copyright (C) 2018-2023 Kenneth S. Kundert
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
